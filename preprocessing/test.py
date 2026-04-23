7686b8643d16aaa0b88c9449d09791b59df197db
gonyihc <chiyong118@gmail.com>
2026-04-23 16:05:02 +0800

    刪除不再使用的檔案，包括 DataTransformer.py、dictionary.py、input.txt、output_results 中的所有輸出檔案及統計檔案

--- preprocessing/test.py
+++
@@ -1,8 +0,0 @@
-from pypinyin import pinyin, Style
-
-while True:
-    text = input()
-    results = pinyin(text, style=Style.BOPOMOFO, heteronym=True)
-
-    # results 的格式會是 [['ㄓㄨㄥˋ', 'ㄔㄨㄥˊ']]
-    print(results[0])
