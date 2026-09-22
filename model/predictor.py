fe91f036fe234373dbf7b2ef7e0bde99c79d323d
KengNSYSU <B123040031@g-mail.nsysu.edu.tw>
2026-09-23 00:46:34 +0800

    len57 58 62 end

--- model/predictor.py
+++ model/predictor.py
@@ -7,8 +7,8 @@ device = None
 src_tokenizer = None
 trg_tokenizer = None
 model = None
-name = 'transcoder_len54_v7.pth'
-model_len = 54
+name = 'transcoder_len62_v5.pth'
+model_len = 55
 
 def initialize():
     global device, src_tokenizer, trg_tokenizer, model
